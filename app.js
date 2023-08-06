a72449f85c1340dfbdbe24eb889fdc75d8164789
Wajahat-404 <109886908+Wajahat-404@users.noreply.github.com>
2023-08-06 12:20:05 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+let a = {
+    average: (a,b) => {
+        console.log((a+b)/2);
+    },
+    percent: (a,b) => {
+        console.log((a/b)*2);
+    }
+}
+
+module.exports = a;
---
+++ index.js
@@ -0,0 +1,3 @@
+let a = require('./app.js');
+
+a.average(3,4);
