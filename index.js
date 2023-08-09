c388b03d11ae988a6da446face55ce828602335e
Wajahat-404 <109886908+Wajahat-404@users.noreply.github.com>
2023-08-09 09:21:03 +0500

    File System updates added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-let a = require('./app.js'); //Common or Old Way to import modules
+// let a = require('./app.js'); //Common or Old Way to import modules
 
 // import a from './app.js'; // New Way to import modules
 
-a.average(3,4);
+// a.average(3,4);
+
